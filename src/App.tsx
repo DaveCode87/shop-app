a1eb54713492058f7340483380991d88fb794b18
davideCoderblock <davide@coderblock.com>
2023-11-22 15:09:04 +0100

    (feat add tailwind and example)

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="page">
+      <div className="title">UIKIT</div>
+        <table className="table-auto w-full hover">
+            <thead>
+            <tr>
+                <th>Song</th>
+                <th>Artist</th>
+                <th>Year</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+            </tr>
+            <tr>
+                <td>Witchy Woman</td>
+                <td>The Eagles</td>
+                <td>1972</td>
+            </tr>
+            <tr>
+                <td>Shining Star</td>
+                <td>Earth, Wind, and Fire</td>
+                <td>1975</td>
+            </tr>
+            </tbody>
+        </table>
+      <br/>
+
+      <input type="text"/>
+      <input type="text" className="error"/>
+
+      <br/>
+
+      <button className="btn">Default</button>
+      <button className="btn" disabled>Default</button>
+      <button className="btn dark">Dark</button>
+      <button className="btn danger">danger</button>
+      <button className="btn primary">primary</button>
+      <button className="btn accent">accent</button>
+      <button className="btn success">success</button>
+      <button className="btn success outline">success</button>
+      <button className="btn outline">outline</button>
+      <button className="btn primary">base</button>
+      <button className="btn primary lg">lg</button>
+    </div>
   )
 }
 
