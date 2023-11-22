f65c4412bfc98985828c215f63355b48fcade224
davideCoderblock <davide@coderblock.com>
2023-11-22 21:26:31 +0100

    (feat create pages and add react router)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div className="page">
-      <div className="title">UIKIT Davide</div>
+      <div className="title">UIKIT</div>
         <table className="table-auto w-full hover">
             <thead>
             <tr>
