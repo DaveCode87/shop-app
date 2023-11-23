082aca76032241cbe6b8c85cea19935d4bc791b0
davideCoderblock <davide@coderblock.com>
2023-11-23 10:05:42 +0100

    (feat add components shop)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
+// NEW
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <App />
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <App />
 )
--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,32 +1,57 @@
-import { Product} from '@/model/product';
 import { useEffect, useState } from 'react';
-
-// NEW
 import { pb } from '../../pocketbase';
+import { Product } from '@/model/product';
+import { ServerError, Spinner } from '@/shared/';
+import { ProductCard } from './components/ProductCard';
+
 
 export function ShopPage() {
-  const [products, setProducts] = useState<Product[]>([])
+  const [products, setProducts] = useState<Product[]>([]);
+  const [pending, setPending] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
+    loadData()
+  }, [])
+
+  function loadData() {
+    setError(false);
+    setPending(true);
+
     pb.collection('products').getList<Product>()
       .then(res => {
-        setProducts(res.items)
+        setError(false);
+        setProducts(res.items);
       })
-  }, [])
+      .catch(() => setError(true))
+      .finally(() => setPending(false))
+  }
+
+  function addToCart(product: Partial<Product>) {
+    console.log(product)
+  }
 
   return (
     <div>
       <h1 className="title">SHOP</h1>
 
+      {pending && <Spinner />}
+      {error && <ServerError />}
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
       {
         products.map(p => {
           return (
-            <div key={p.id}>
-             • {p.name}
-            </div>
+            <ProductCard
+              key={p.id}
+              product={p}
+              onAddToCart={addToCart}
+            />
           )
         })
       }
+      </div>
+
     </div>
   )
 }
